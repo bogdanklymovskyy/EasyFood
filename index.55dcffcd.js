f5f3aa43b227e6e7e181b7935c63514fe1ee7fc6
bogdanklymovskyy <106254415+bogdanklymovskyy@users.noreply.github.com>
2022-09-01 11:24:06 +0000

    Deploying to gh-pages from @ bogdanklymovskyy/EasyFood@dcb2b7fba9fc20faeae3f31ca4bf3ba997d9e97e 🚀

---
+++ index.55dcffcd.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-mob-open]"),closeModalBtn:document.querySelector("[data-mob-close]"),modal:document.querySelector("[data-mob]")};function o(){e.modal.classList.toggle("is-open")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.55dcffcd.js.map
